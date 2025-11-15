fa9ba33f35eb77d38cb2fd2ed86890c592e0d1ba
Erivan011 <166454681+Erivan011@users.noreply.github.com>
2025-11-14 21:21:19 -0300

    Delete logistics-solution-for-erivan directory

--- logistics-solution-for-erivan/utils/links.py
+++
@@ -1,6 +0,0 @@
-
-LINKS = {
-    'repo': '[SUBSTITUA_PELO_LINK_DO_REPO](https://github.com/SEU_USUARIO/SEU_REPO)',
-    'deploy': '[SUBSTITUA_PELO_LINK_DO_DEPLOY](https://example.streamlit.app)',
-    'video': '[SUBSTITUA_PELO_LINK_DO_VIDEO](https://youtube.com)'
-}
